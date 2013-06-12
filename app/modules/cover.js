e63bddedae8f6fda487672f8d237303fda8bff82
James Burns <jameburn@gmail.com>
2013-06-11 17:55:24 -0700

    add background image upload to user profile

--- app/modules/cover.js
+++ app/modules/cover.js
@@ -65,8 +65,53 @@ function( app ) {
             return false;
         },
 
-        onChangeBackgroundImage: function() {
-            console.log('bg image updated')
+        onChangeBackgroundImage: function( event ) {
+            
+            var fileInput = event.target,
+                imageData,
+                _this = this;
+
+            imageData = new FormData();
+            imageData.append( "file", fileInput.files[0] );
+
+            var updateProgress = function( e ){
+                var w = e.loaded * 141 / e.total;
+                _this.$('.upload-progress').clearQueue().animate ({ "width": w + "px"}, 1000);
+                if(  w == 141 ) {
+                    _this.$('.upload-progress').clearQueue().animate ({ "width": "283px"}, 10000);
+                }
+
+            };
+
+
+            $.ajax({
+                url: app.metadata.mediaServer + "image",
+                type: "POST",
+                data: imageData,
+                dataType: "json",
+                processData: false,
+                contentType: false,
+                fileElementId: "imagefile",
+                
+                xhr: function() {  // custom xhr
+                    myXhr = $.ajaxSettings.xhr();
+                    if(myXhr.upload){ // check if upload property exists
+                        myXhr.upload.addEventListener('progress', updateProgress, false); // for handling the progress of the upload
+                    }
+                    return myXhr;
+                },
+                
+                success: function( data ) {
+                    
+                    this.model.save({
+                        background_image_url: data.fullsize_url
+                    });
+                    this.$(".cover").css("background-image", data.fullsize_url);
+                }.bind(this)
+            });
+
+
+
         },
 
         onChangeProfileImage: function() {
