705b110572bea9ec67b6931af66e6995f6d344fc
jbergen <jbergen@gmail.com>
2013-06-11 21:04:34 -0700

    release

--- dist/debug/templates.js
+++ dist/debug/templates.js
@@ -107,7 +107,7 @@ var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
 with(obj||{}){
 __p+='<iframe src="'+
 (path )+
-'" endPage="true" hideChrome="true" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>\n<a href="#" class="modal-close">×</a>';
+'" endPage="true" hideChrome="true" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>\n<div class="modal-close">×</div>';
 }
 return __p;
 };
